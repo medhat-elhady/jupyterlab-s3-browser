968d12201a9a4ee5658823fa24cfd0dea8aa0fbd
medhat-elhady <medhat.elhady@rackspace.com>
2024-08-13 22:02:10 +0300

    update s3 bucket

--- jupyterlab_s3_browser/handlers.py
+++ jupyterlab_s3_browser/handlers.py
@@ -1,6 +1,3 @@
-"""
-Placeholder
-"""
 import base64
 import json
 import logging
@@ -9,36 +6,30 @@ from pathlib import Path
 import boto3
 import s3fs
 import tornado
-from botocore.exceptions import NoCredentialsError
+from botocore.exceptions import NoCredentialsError, ClientError
 from jupyter_server.base.handlers import APIHandler
 from jupyter_server.utils import url_path_join
 
 
 class DirectoryNotEmptyException(Exception):
     """Raise for attempted deletions of non-empty directories"""
-
     pass
 
 
 def create_s3fs(config):
-
     if config.endpoint_url and config.client_id and config.client_secret:
-
         return s3fs.S3FileSystem(
             key=config.client_id,
             secret=config.client_secret,
             token=config.session_token,
             client_kwargs={"endpoint_url": config.endpoint_url},
         )
-
     else:
         return s3fs.S3FileSystem()
 
 
 def create_s3_resource(config):
-
     if config.endpoint_url and config.client_id and config.client_secret:
-
         return boto3.resource(
             "s3",
             aws_access_key_id=config.client_id,
@@ -46,52 +37,46 @@ def create_s3_resource(config):
             aws_session_token=config.session_token,
             endpoint_url=config.endpoint_url,
         )
-
     else:
         return boto3.resource("s3")
-    
-def check_bucket_access(bucket_name):
-    # Initialize a session using Amazon S3
-    s3 = boto3.client('s3')
-    
-    try:
-        # Try to access the bucket by listing its contents
-        s3.head_bucket(Bucket=bucket_name)
-        return True
-    except:
-        return False
+
 
 def _test_aws_s3_role_access():
     """
-    Checks if we have access to AWS S3 through role-based access
+    Checks if we have access to AWS S3 through role-based access and lists only accessible buckets.
     """
-    test = boto3.resource("s3")
-    all_buckets = test.buckets.all()
-    result = [
-        {"name": bucket.name + "/", "path": bucket.name + "/", "type": "directory"}
-        for bucket in all_buckets
-    ]
-    return result
+    s3 = boto3.client("s3")
+    all_buckets = s3.list_buckets()["Buckets"]
+    accessible_buckets = []
+
+    for bucket in all_buckets:
+        try:
+            # Try to list objects in the bucket to verify access
+            s3.list_objects_v2(Bucket=bucket["Name"], MaxKeys=1)
+            accessible_buckets.append(
+                {"name": bucket["Name"] + "/", "path": bucket["Name"] + "/", "type": "directory"}
+            )
+        except ClientError as e:
+            # Ignore access denied errors
+            if e.response["Error"]["Code"] == "AccessDenied":
+                logging.info(f"Access denied for bucket: {bucket['Name']}")
+            else:
+                raise e
+
+    return accessible_buckets
 
 
 def has_aws_s3_role_access():
     """
     Returns true if the user has access to an aws S3 bucket
     """
-
-    # avoid making requests to AWS if the user's ~/.aws/credentials file has credentials for a different provider,
-    # e.g. https://cloud.ibm.com/docs/cloud-object-storage?topic=cloud-object-storage-aws-cli#aws-cli-config
     aws_credentials_file = Path("{}/.aws/credentials".format(Path.home()))
     if aws_credentials_file.exists():
         with aws_credentials_file.open() as credentials_file:
             for line in credentials_file.readlines():
                 if line.startswith("aws_access_key_id"):
-                    access_key_id = line.split("=", 1)[1]
-                    # aws keys reliably start with AKIA for long-term or ASIA for short-term
-                    if not access_key_id.startswith(
-                        "AKIA"
-                    ) and not access_key_id.startswith("ASIA"):
-                        # if any keys are not valid AWS keys, don't try to authenticate
+                    access_key_id = line.split("=", 1)[1].strip()
+                    if not access_key_id.startswith("AKIA") and not access_key_id.startswith("ASIA"):
                         logging.info(
                             "Found invalid AWS aws_access_key_id in ~/.aws/credentials file, "
                             "will not attempt to authenticate through ~/.aws/credentials."
@@ -130,10 +115,6 @@ def test_s3_credentials(endpoint_url, client_id, client_secret, session_token):
 
 
 class AuthHandler(APIHandler):  # pylint: disable=abstract-method
-    """
-    handle api requests to change auth info
-    """
-
     @property
     def config(self):
         return self.settings["s3_config"]
@@ -141,15 +122,13 @@ class AuthHandler(APIHandler):  # pylint: disable=abstract-method
     @tornado.web.authenticated
     def get(self, path=""):
         """
-        Checks if the user is already authenticated
-        against an s3 instance.
+        Checks if the user is already authenticated against an s3 instance.
         """
         authenticated = False
         if has_aws_s3_role_access():
             authenticated = True
 
         if not authenticated:
-
             try:
                 config = self.config
                 if config.endpoint_url and config.client_id and config.client_secret:
@@ -160,44 +139,13 @@ class AuthHandler(APIHandler):  # pylint: disable=abstract-method
                         config.session_token,
                     )
                     logging.debug("...successfully authenticated")
-
-                    # If no exceptions were encountered during testS3Credentials,
-                    # then assume we're authenticated
                     authenticated = True
 
             except Exception as err:
-                # If an exception was encountered,
-                # assume that we're not yet authenticated
-                # or invalid credentials were provided
                 logging.debug("...failed to authenticate")
                 logging.debug(err)
 
         self.finish(json.dumps({"authenticated": authenticated}))
-
-    @tornado.web.authenticated
-    def post(self, path=""):
-        """
-        Sets s3 credentials.
-        """
-
-        try:
-            req = json.loads(self.request.body)
-            endpoint_url = req["endpoint_url"]
-            client_id = req["client_id"]
-            client_secret = req["client_secret"]
-            session_token = req["session_token"]
-
-            test_s3_credentials(endpoint_url, client_id, client_secret, session_token)
-
-            self.config.endpoint_url = endpoint_url
-            self.config.client_id = client_id
-            self.config.client_secret = client_secret
-            self.config.session_token = session_token
-
-            self.finish(json.dumps({"success": True}))
-        except Exception as err:
-            logging.info("unable to authenticate using credentials")
-            self.finish(json.dumps({"success": False, "message": str(err)}))
 
 
 class S3ResourceNotFoundException(Exception):
@@ -213,10 +161,6 @@ def convertS3FStoJupyterFormat(result):
 
 
 class S3Handler(APIHandler):
-    """
-    Handles requests for getting S3 objects
-    """
-
     @property
     def config(self):
         return self.settings["s3_config"]
@@ -231,7 +175,6 @@ class S3Handler(APIHandler):
         and directories/prefixes based on the path.
         """
         path = path[1:]
-        #  logging.info("GET {}".format(path))
 
         try:
             if not self.s3fs:
@@ -239,9 +182,7 @@ class S3Handler(APIHandler):
 
             self.s3fs.invalidate_cache()
 
-            if (path and not path.endswith("/")) and (
-                "X-Custom-S3-Is-Dir" not in self.request.headers
-            ):  # TODO: replace with function
+            if (path and not path.endswith("/")) and ("X-Custom-S3-Is-Dir" not in self.request.headers):
                 with self.s3fs.open(path, "rb") as f:
                     result = {
                         "path": path,
@@ -249,18 +190,16 @@ class S3Handler(APIHandler):
                         "content": base64.encodebytes(f.read()).decode("ascii"),
                     }
             else:
-                raw_result = list(
-                    map(convertS3FStoJupyterFormat, self.s3fs.listdir(path))
-                )
+                raw_result = list(map(convertS3FStoJupyterFormat, self.s3fs.listdir(path)))
                 result = list(filter(lambda x: x["name"] != "", raw_result))
-                result = list(filter(lambda x: check_bucket_access(x["name"]), result))
+
         except S3ResourceNotFoundException as e:
             result = {
                 "error": 404,
                 "message": "The requested resource could not be found.",
             }
         except Exception as e:
-            logging.error("Exception encountered during GET {}: {}".format(path, e))
+            logging.error(f"Exception encountered during GET {path}: {e}")
             result = {"error": 500, "message": str(e)}
 
         self.finish(json.dumps(result))
@@ -281,14 +220,9 @@ class S3Handler(APIHandler):
 
             if "X-Custom-S3-Copy-Src" in self.request.headers:
                 source = self.request.headers["X-Custom-S3-Copy-Src"]
-
-                # copying issue is because of dir/file mixup?
                 if "/" not in source:
                     path = path + "/.keep"
-
-                #  logging.info("copying {} -> {}".format(source, path))
                 self.s3fs.cp(source, path, recursive=True)
-                # why read again?
                 with self.s3fs.open(path, "rb") as f:
                     result = {
                         "path": path,
@@ -297,10 +231,7 @@ class S3Handler(APIHandler):
                     }
             elif "X-Custom-S3-Move-Src" in self.request.headers:
                 source = self.request.headers["X-Custom-S3-Move-Src"]
-
-                #  logging.info("moving {} -> {}".format(source, path))
                 self.s3fs.move(source, path, recursive=True)
-                # why read again?
                 with self.s3fs.open(path, "rb") as f:
                     result = {
                         "path": path,
@@ -309,17 +240,14 @@ class S3Handler(APIHandler):
                     }
             elif "X-Custom-S3-Is-Dir" in self.request.headers:
                 path = path.lower()
-                if not path[-1] == "/":
+                if not path.endswith("/"):
                     path = path + "/"
-
-                #  logging.info("creating new dir: {}".format(path))
                 self.s3fs.mkdir(path)
                 self.s3fs.touch(path + ".keep")
             elif self.request.body:
                 request = json.loads(self.request.body)
                 with self.s3fs.open(path, "w") as f:
                     f.write(request["content"])
-                    # todo: optimize
                     result = {
                         "path": path,
                         "type": "file",
@@ -327,12 +255,14 @@ class S3Handler(APIHandler):
                     }
 
         except S3ResourceNotFoundException as e:
-            #  logging.info(e)
             result = {
                 "error": 404,
                 "message": "The requested resource could not be found.",
             }
+        except DirectoryNotEmptyException as e:
+            result = {"error": 400, "message": "Directory not empty"}
         except Exception as e:
+            logging.error("error while deleting")
             logging.error(e)
             result = {"error": 500, "message": str(e)}
 
